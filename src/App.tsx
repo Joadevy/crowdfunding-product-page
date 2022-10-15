6fa7eaeb4f40deefd0fe50c470fbfcfbbfb3bdc9
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-10-15 18:40:11 -0300

    refactor: header have their own modal state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import type { infoBackers } from "./types";
+import type { infoReward } from "./Components/Reward";
+
 import { useEffect, useState } from "react";
 
 import Header from "./Components/Header";
 import StatCard from "./Components/StatCard";
-import { infoBackers } from "./types";
+import Reward from "./Components/Reward";
 
 function App() {
   const [bookmarked, setBookmarked] = useState<boolean>(false);
-  const [navbarModal, toggleNavbarModal] = useState<boolean>(false);
   const [data, setData] = useState<infoBackers>({
     amountBacked: 0,
     totalBackers: 0,
     daysLeft: 0,
   });
+  const [pledges, setPledges] = useState<infoReward[]>([]);
   const [status, setStatus] = useState<"loading" | "success">("loading");
   const [amountBacked, setAmountBacked] = useState<number>(0);
 
@@ -37,11 +40,14 @@ function App() {
 
   const requestData = async () => {
     const data = await fetch("data.json");
-    const res = await data.json();
+    const dataRes = await data.json();
+    const pledges = await fetch("pledges.json");
+    const dataPledges = await pledges.json();
 
-    if (res) {
-      setData(res);
-      setAmountBacked(res.amountBacked);
+    if (dataRes && dataPledges) {
+      setPledges(dataPledges);
+      setData(dataRes);
+      setAmountBacked(dataRes.amountBacked);
       setStatus("success");
     }
   };
@@ -49,10 +55,10 @@ function App() {
   if (status === "loading") return <h2>Loading...</h2>;
 
   return (
-    <div className={navbarModal ? "App" : "App"}>
-      <Header navbarModal={navbarModal} toggleNavbarModal={toggleNavbarModal} />
-      <main className="relative flex flex-col gap-10 mb-10">
-        <header className="mx-8 shadow-md mt-[-35px] flex flex-col px-5 pb-7 gap-5 items-center rounded-lg bg-slate-100">
+    <div>
+      <Header />
+      <main className="mx-10 relative flex flex-col gap-10 mb-28">
+        <header className="shadow-md mt-[-35px] flex flex-col px-5 pb-7 gap-5 items-center rounded-lg bg-slate-100">
           <div className="mt-[-30px] max-w-max">
             <img alt="" src="images/logo-mastercraft.svg" srcSet="" />
           </div>
@@ -88,7 +94,7 @@ function App() {
           </div>
         </header>
 
-        <article className="mx-8 bg-slate-100 shadow-md p-5 flex flex-col gap-8 items-center justify-center">
+        <article className="bg-slate-100 shadow-md py-5 flex flex-col gap-8 items-center justify-center">
           <StatCard data={amountBacked} desc="of $100,000 backed" />
           <StatCard data={data.totalBackers} desc="total backers" />
           <StatCard data={data.daysLeft} desc="days left" />
@@ -97,6 +103,27 @@ function App() {
               className="bg-primary-moderate-cyan h-4 rounded-xl"
               style={{ width: `${(amountBacked * 100) / 100000}%` }}
             />
+          </div>
+        </article>
+
+        <article>
+          <h2 className="text-xl font-bold mb-5">About this project</h2>
+          <p className="mb-5 text-neutral-dark-gray">
+            The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
+            platform that elevates your screen to a more comfortable viewing
+            height. Placing your monitor at eye level has the potential to
+            improve your posture and make you more comfortable while at work,
+            helping you stay focused on the task at hand.
+          </p>
+          <p className="text-neutral-dark-gray">
+            Featuring artisan craftsmanship, the simplicity of design creates
+            extra desk space below your computer to allow notepads, pens, and
+            USB sticks to be stored under the stand.
+          </p>
+          <div className="mt-10 flex flex-col gap-5">
+            {pledges.map((pledge: infoReward) => (
+              <Reward key={pledge.title} reward={pledge} />
+            ))}
           </div>
         </article>
       </main>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,50 +1,46 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import Modal from "./Moda";
 
-type props = {
-  navbarModal: boolean;
-  toggleNavbarModal: (_x: boolean) => void;
-};
+const Header: FC = () => {
+  const [showModal, toggleModal] = useState<boolean>(false);
 
-const Header: FC<props> = ({
-  navbarModal: showModal,
-  toggleNavbarModal: toggleModal,
-}) => (
-  <header className="flex relative justify-between items-start px-6 py-9 h-80 bg-mobile-hero bg-cover">
-    <div className="flex justify-between w-full items-center bg-none shadow-hero h-0">
-      <img alt="" src="images/logo.svg" />
-      <nav>
-        <button
-          className="justify-self-center lg:hidden"
-          onClick={() => {
-            toggleModal(!showModal);
-          }}
-        >
-          {showModal ? (
-            <img alt="" src="images/icon-close-menu.svg" />
-          ) : (
-            <img alt="" src="images/icon-hamburger.svg" />
-          )}
-        </button>
-      </nav>
-    </div>
-    {showModal ? (
-      <Modal>
-        <ol className="z-10 absolute w-5/6 top-24 right-0 left-0 m-auto bg-slate-50 p-6 rounded-xl shadow-modal font-semibold text-lg flex flex-col gap-10">
-          <li>
-            <a href="#c">About</a>
-          </li>
-          <li>
-            <a href="#b">Discover</a>
-          </li>
-          <li>
-            <a href="#a">Get started</a>
-          </li>
-        </ol>
-      </Modal>
-    ) : null}
-  </header>
-);
+  return (
+    <header className="flex relative justify-between items-start px-6 py-9 h-80 bg-mobile-hero bg-cover">
+      <div className="flex justify-between w-full items-center bg-none shadow-hero h-0">
+        <img alt="" src="images/logo.svg" />
+        <nav>
+          <button
+            className="justify-self-center lg:hidden"
+            onClick={() => {
+              toggleModal(!showModal);
+            }}
+          >
+            {showModal ? (
+              <img alt="" src="images/icon-close-menu.svg" />
+            ) : (
+              <img alt="" src="images/icon-hamburger.svg" />
+            )}
+          </button>
+        </nav>
+      </div>
+      {showModal ? (
+        <Modal>
+          <ol className="z-10 absolute w-5/6 top-24 right-0 left-0 m-auto bg-slate-50 p-6 rounded-xl shadow-modal font-semibold text-lg flex flex-col gap-10">
+            <li>
+              <a href="#c">About</a>
+            </li>
+            <li>
+              <a href="#b">Discover</a>
+            </li>
+            <li>
+              <a href="#a">Get started</a>
+            </li>
+          </ol>
+        </Modal>
+      ) : null}
+    </header>
+  );
+};
 
 export default Header;
